4563c44e1f1b140f78812480a138d54aa16cb975
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2019-09-13 11:15:11 -0300

    Feature/#1 (#5)
    * fix(Convair): Added SCM Variables to ENV
    
    * fix(Convair): Added SCM Variables to ENV
    
    * Added Jenkins ENV Object, to Convair Closure
    
    * Implemented #1 dynamic docker agents

--- vars/Convair.groovy
+++ vars/Convair.groovy
@@ -1,29 +1,42 @@
 interface ConvairStage {
     Closure shouldRun
     Closure run
+    String image
 }
 
 
-def call(Closure body){
-    def parameters = [:]
+def call(Closure body) {
+    def parameters = [env: env]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = parameters
-
-    body()
-
     Map variables = parameters.variables ?: [:]
     variables.each {
         env[it.key] = it.value
     }
+    body()
+
+
     Map<String, ConvairStage> selectedStages = parameters.selectedStages
 
-    node(parameters.selectedAgent) {
-        stage("Initialize"){
+    def nodeLabel = parameters.selectedAgent instanceof String ? parameters.selectedAgent : null
+    def nodeParameters = [:]
+    if(parameters.selectedAgent instanceof Closure){
+        parameters.selectedAgent.delegate = nodeParameters
+        parameters.selectedAgent.resolveStrategy = Closure.DELEGATE_FIRST
+        parameters.selectedAgent()
+        nodeLabel = nodeParameters.label ?: nodeLabel
+
+
+    }
+    node(nodeLabel) {
+
+
+        stage("Initialize") {
             println parameters
             sh "env"
         }
         def scmVars
-        stage("Checkout SCM"){
+        stage("Checkout SCM") {
             scmVars = checkout scm
             scmVars.each {
                 env[it.key] = it.value
@@ -39,23 +52,32 @@ def call(Closure body){
                 myStage.value.shouldRun.resolveStrategy = Closure.DELEGATE_FIRST
                 myStage.value.run.resolveStrategy = Closure.DELEGATE_FIRST
 
-                if(myStage.value.shouldRun()){
-                    stage(myStage.key){
-                        myStage.value.run()
+                if (myStage.value.shouldRun()) {
+                    def dockerImage = myStage.value.image ?: nodeParameters.image
+                    if (dockerImage) {
+                        docker.image(dockerImage).inside {
+                            stage(myStage.key) {
+                                myStage.value.run()
+                            }
+                        }
+                    } else {
+                        stage(myStage.key) {
+                            myStage.value.run()
+                        }
                     }
                 } else {
                     println "Stage ${myStage.key} skipped"
                 }
             }
             currentBuild.result = 'SUCCESS'
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace()
             currentBuild.result = 'FAILURE'
         }
 
-        if(currentBuild.result == "FAILURE"){
-            if(parameters.onFailure && parameters.onFailure instanceof Closure){
-                stage("On Failure"){
+        if (currentBuild.result == "FAILURE") {
+            if (parameters.onFailure && parameters.onFailure instanceof Closure) {
+                stage("On Failure") {
                     parameters.onFailure.delegate = scriptClosure
                     parameters.onFailure.resolveStrategy = Closure.DELEGATE_FIRST
                     parameters.onFailure()
@@ -63,9 +85,9 @@ def call(Closure body){
             }
         }
 
-        if(currentBuild.result == "SUCCESS"){
-            if(parameters.onSuccess && parameters.onSuccess instanceof Closure){
-                stage("On Success"){
+        if (currentBuild.result == "SUCCESS") {
+            if (parameters.onSuccess && parameters.onSuccess instanceof Closure) {
+                stage("On Success") {
                     parameters.onSuccess.delegate = scriptClosure
                     parameters.onSuccess.resolveStrategy = Closure.DELEGATE_FIRST
                     parameters.onSuccess()
@@ -73,8 +95,8 @@ def call(Closure body){
             }
         }
 
-        if(parameters.always && parameters.always instanceof Closure){
-            stage("Always"){
+        if (parameters.always && parameters.always instanceof Closure) {
+            stage("Always") {
                 parameters.always.delegate = scriptClosure
                 parameters.always.resolveStrategy = Closure.DELEGATE_FIRST
                 parameters.always()
